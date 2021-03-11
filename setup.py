a0a9aba1e7186d2c2dd0276f9e7a5304073891fd
Maksym Brilenkov <maxim.brilenkov@gmail.com>
2021-03-11 21:46:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from distutils.core import setup
 
 setup(name='funnyplotting',
-      version='1.0',
+      version='1.0.0',
       description='plotting 2D/3D graphs',
-      author='Maxim Brilenkov',
+      author='Maksym Brilenkov',
       author_email='maxim.brilenkov@gmail.com',
       url='https://github.com/hroney/funnyplotting.git',
       scripts=['funnyplotting']
